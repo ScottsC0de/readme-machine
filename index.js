525f729fd5e5931fc4bc5c444423ef5884168e7c
scottsc0de <scott5902@gmail.com>
2022-11-10 08:42:25 -0500

    added working badge icons for all licenses

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const generateReadMe = ({ title, description, installation, usage, contributing,
     // indenting was preventing headers from working
     `# ${title}
 
+![License](https://img.shields.io/badge/License-${license}.svg)
+
 ## Table of Contents
+
 - [Description](#description-id)
 - [Installation](#installation-id)
 - [Usage](#usage-id)
@@ -64,22 +67,22 @@ inquirer
             name: 'usage',
         },
         {
-            type: 'checkbox',
+            type: 'list',
             message: 'Please choose a license from the following:',
             choices: [
-                { name: 'Apache License 2.0', value: 'Apache License 2.0' },
-                { name: 'GNU General Public License v3.0', value: 'GNU General Public License v3.0' },
-                { name: 'MIT License', value: 'MIT License' },
-                { name: 'BSD 2-Clause "Simplified" License', value: 'BSD 2-Clause "Simplified" License' },
-                { name: 'BSD 3-Clause "New" or "Revised" License', value: 'BSD 3-Clause "New" or "Revised" License' },
-                { name: 'Boost Software License 1.0', value: 'Boost Software License 1.0' },
-                { name: 'Creative Commons Zero v1.0 Universal', value: 'Creative Commons Zero v1.0 Universal' },
-                { name: 'Eclipse Public License 2.0', value: 'Eclipse Public License 2.0' },
-                { name: 'GNU Affero General Public License v3.0', value: 'GNU Affero General Public License v3.0' },
-                { name: 'GNU General Public License v2.0', value: 'GNU General Public License v2.0' },
-                { name: 'GNU Lesser General Public License v2.1', value: 'GNU Lesser General Public License v2.1' },
-                { name: 'Mozilla Public License 2.0', value: 'Mozilla Public License 2.0' },
-                { name: 'The Unlicense', value: 'The Unlicense' }
+                { name: 'Apache License 2.0', value: 'Apache_2.0-blue' },
+                { name: 'GNU General Public License v3.0', value: 'GPLv3-blue' },
+                { name: 'MIT License', value: 'MIT-yellow' },
+                { name: 'BSD 2-Clause "Simplified" License', value: 'BSD_2--Clause-orange' },
+                { name: 'BSD 3-Clause "New" or "Revised" License', value: 'BSD_3--Clause-blue' },
+                { name: 'Boost Software License 1.0', value: 'Boost_1.0-lightblue' },
+                { name: 'Creative Commons Zero v1.0 Universal', value: 'CC0_1.0-lightgrey' },
+                { name: 'Eclipse Public License 2.0', value: 'EPL_1.0-red' },
+                { name: 'GNU Affero General Public License v3.0', value: 'AGPL_v3-blue' },
+                { name: 'GNU General Public License v2.0', value: 'GPL_v2-blue' },
+                { name: 'GNU Lesser General Public License v3.0', value: 'LGPL_v3-blue' },
+                { name: 'Mozilla Public License 2.0', value: 'MPL_2.0-brightgreen' },
+                { name: 'The Unlicense', value: 'Unlicense-blue' }
             ],
             name: 'license',
         },
@@ -110,7 +113,11 @@ inquirer
 
         fs.writeFile('README.md', readMeMachine, (err) =>
             err ? console.log(err) : console.log("Saved!")
-        )
+        );
+
+        // function createLicenseBagde(license) {
+
+        // }
     });
 
 // TODO: Create a function to write README file
@@ -121,6 +128,26 @@ inquirer
 
 // Function call to initialize app
 // init();
+
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+// function renderLicenseBadge(license) { }
+
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) { }
+
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) { }
+
+// // TODO: Create a function to generate markdown for README
+// function generateMarkdown(data) {
+//     return `# ${data.title}
+// `;
+// }
+
+// module.exports = generateMarkdown;
 
 // GIVEN a command-line application that accepts user input
 // WHEN I am prompted for information about my application repository
