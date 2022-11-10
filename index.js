775146d8a8a3ec3294191be51dd9a0c8a09bce16
scottsc0de <scott5902@gmail.com>
2022-11-10 09:44:08 -0500

    full working functionality

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-// TODO: Create a function to initialize app
-// function init() { }
+function init() {
 
-// Function call to initialize app
-// init();
+    const inquirer = require('inquirer');
+    const fs = require('fs');
 
-// module.exports = generateMarkdown;
-
-const inquirer = require('inquirer');
-const fs = require('fs');
-
-const generateReadMe = ({ title, description, installation, usage, contributing, test, license, license_confirm, github, email }) =>
-    // indenting was preventing h2s from working
-    `# ${title}
+    const generateReadMe = ({ title, description, installation, usage, contributing, test, license, license_confirm, github, email }) =>
+        // indenting was preventing h2s from working
+        `# ${title}
 
 ![License](https://img.shields.io/badge/License-${license}.svg)
 
@@ -47,81 +41,85 @@ ${test}
 <a href="https://github.com/${github}">${github}</a>\n
 Send an email to **${email}** with any additional questions`
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'What is the title of your project?',
-            name: 'title',
-        },
-        {
-            type: 'input',
-            message: 'Please enter a description of your project',
-            name: 'description',
-        },
-        {
-            type: 'input',
-            message: 'Please provide installation instructions for your project',
-            name: 'installation',
-        },
-        {
-            type: 'input',
-            message: 'Please enter usage information about your project',
-            name: 'usage',
-        },
-        {
-            type: 'list',
-            message: 'Please choose a license from the following:',
-            choices: [
-                { name: 'Apache License 2.0', value: 'Apache_2.0-blue' },
-                { name: 'GNU General Public License v3.0', value: 'GPLv3-blue' },
-                { name: 'MIT License', value: 'MIT-yellow' },
-                { name: 'BSD 2-Clause "Simplified" License', value: 'BSD_2--Clause-orange' },
-                { name: 'BSD 3-Clause "New" or "Revised" License', value: 'BSD_3--Clause-blue' },
-                { name: 'Boost Software License 1.0', value: 'Boost_1.0-lightblue' },
-                { name: 'Creative Commons Zero v1.0 Universal', value: 'CC0_1.0-lightgrey' },
-                { name: 'Eclipse Public License 2.0', value: 'EPL_1.0-red' },
-                { name: 'GNU Affero General Public License v3.0', value: 'AGPL_v3-blue' },
-                { name: 'GNU General Public License v2.0', value: 'GPL_v2-blue' },
-                { name: 'GNU Lesser General Public License v3.0', value: 'LGPL_v3-blue' },
-                { name: 'Mozilla Public License 2.0', value: 'MPL_2.0-brightgreen' },
-                { name: 'The Unlicense', value: 'Unlicense-blue' }
-            ],
-            name: 'license',
-            // if there is no license, return an empty string
-        },
-        {
-            type: 'input',
-            message: 'Please enter your license selection to confirm',
-            name: 'license_confirm'
-        },
-        {
-            type: 'input',
-            message: 'Please enter any contribution guidelines for your project',
-            name: 'contributing',
-        },
-        {
-            type: 'input',
-            message: 'Please enter test intructions for your project',
-            name: 'test',
-        },
-        {
-            type: 'input',
-            message: 'What is your GitHub username?',
-            name: 'github',
-        },
-        {
-            type: 'input',
-            message: 'What is your email?',
-            name: 'email',
-        }
-    ])
-    .then((answers) => {
-        const readMeMachine = generateReadMe(answers);
-        console.log(answers);
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is the title of your project?',
+                name: 'title',
+            },
+            {
+                type: 'input',
+                message: 'Please enter a description of your project',
+                name: 'description',
+            },
+            {
+                type: 'input',
+                message: 'Please provide installation instructions for your project',
+                name: 'installation',
+            },
+            {
+                type: 'input',
+                message: 'Please enter usage information about your project',
+                name: 'usage',
+            },
+            {
+                type: 'list',
+                message: 'Please choose a license from the following:',
+                choices: [
+                    { name: 'Apache License 2.0', value: 'Apache_2.0-blue' },
+                    { name: 'GNU General Public License v3.0', value: 'GPLv3-blue' },
+                    { name: 'MIT License', value: 'MIT-yellow' },
+                    { name: 'BSD 2-Clause "Simplified" License', value: 'BSD_2--Clause-orange' },
+                    { name: 'BSD 3-Clause "New" or "Revised" License', value: 'BSD_3--Clause-blue' },
+                    { name: 'Boost Software License 1.0', value: 'Boost_1.0-lightblue' },
+                    { name: 'Creative Commons Zero v1.0 Universal', value: 'CC0_1.0-lightgrey' },
+                    { name: 'Eclipse Public License 2.0', value: 'EPL_1.0-red' },
+                    { name: 'GNU Affero General Public License v3.0', value: 'AGPL_v3-blue' },
+                    { name: 'GNU General Public License v2.0', value: 'GPL_v2-blue' },
+                    { name: 'GNU Lesser General Public License v3.0', value: 'LGPL_v3-blue' },
+                    { name: 'Mozilla Public License 2.0', value: 'MPL_2.0-brightgreen' },
+                    { name: 'The Unlicense', value: 'Unlicense-blue' }
+                ],
+                name: 'license',
+            },
+            {
+                type: 'input',
+                message: 'Please enter your license selection to confirm',
+                name: 'license_confirm'
+            },
+            {
+                type: 'input',
+                message: 'Please enter any contribution guidelines for your project',
+                name: 'contributing',
+            },
+            {
+                type: 'input',
+                message: 'Please enter test intructions for your project',
+                name: 'test',
+            },
+            {
+                type: 'input',
+                message: 'What is your GitHub username?',
+                name: 'github',
+            },
+            {
+                type: 'input',
+                message: 'What is your email?',
+                name: 'email',
+            }
+        ])
+        .then((answers) => {
+            const readMeMachine = generateReadMe(answers);
+            console.log(answers);
 
-        fs.writeFile('README.md', readMeMachine, (err) =>
-            err ? console.log(err) : console.log("Saved!")
-        );
-    });
+            fs.writeFile('README.md', readMeMachine, (err) =>
+                err ? console.log(err) : console.log("Saved!")
+            );
 
+            // module.exports = readMeMachine;
+
+        });
+};
+
+init();
